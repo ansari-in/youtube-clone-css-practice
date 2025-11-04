ef08828711443a2fc6490c87759152bb739abd64
AnsariIn <ansariintesab048@gmail.com>
2025-11-04 14:16:39 +0530

    first commit

---
+++ js/explore.js
@@ -0,0 +1,253 @@
+/*
+function colorred() {
+    
+    let rdcr = document.getElementById("fdjh_sadggh");
+    if (rdcr.style.color == "black"){
+        rdcr.style.color = "yellow";
+    }
+    else {
+        rdcr.style.color = "red";
+    }
+}
+function colorred2() {
+    let rdcr2 = document.getElementById("fdjh_sadggh2");
+    if (rdcr2.style.color == "black"){
+        rdcr2.style.color = "yellow";
+    }
+    else {
+        rdcr2.style.color = "red";
+    }
+}
+function colorred3() {
+    let rdcr3 = document.getElementById("fdjh_sadggh3");
+    if (rdcr3.style.color == "black"){
+        rdcr3.style.color = "yellow";
+    }
+    else {
+        rdcr3.style.color = "red";
+    }
+}
+function colorred4() {
+    let rdcr4 = document.getElementById("fdjh_sadggh4");
+    if (rdcr4.style.color == "black"){
+        rdcr4.style.color = "yellow";
+    }
+    else {
+        rdcr4.style.color = "red";
+    }
+}
+function colorred5() {
+    let rdcr5 = document.getElementById("fdjh_sadggh5");
+    if (rdcr5.style.color == "black"){
+        rdcr5.style.color = "yellow";
+    }
+    else {
+        rdcr5.style.color = "red";
+    }
+}
+*/
+
+function closeadd() {
+    var btncl = document.getElementsByClassName("youtubedgjsa_0154")[0]
+    btncl.style.display= "none";
+}
+
+function ShowAndHide() {
+    var wor = document.getElementById('sadf_asgjhasasqw');
+    // document.getElementById("hidshowjafsdg_dgha").style.background = "red";
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+   
+    
+}
+
+function ShowAndHide2() {
+    var wor = document.getElementById('sadf_asgjhasasqw2');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide3() {
+    var wor = document.getElementById('sadf_asgjhasasqw3');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide4() {
+    var wor = document.getElementById('sadf_asgjhasasqw4');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide5() {
+    var wor = document.getElementById('sadf_asgjhasasqw5');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide6() {
+    var wor = document.getElementById('sadf_asgjhasasqw6');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide7() {
+    var wor = document.getElementById('sadf_asgjhasasqw7');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide8() {
+    var wor = document.getElementById('sadf_asgjhasasqw8');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide9() {
+    var wor = document.getElementById('sadf_asgjhasasqw9');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide10() {
+    var wor = document.getElementById('sadf_asgjhasasqw10');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide11() {
+    var wor = document.getElementById('sadf_asgjhasasqw11');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide12() {
+    var wor = document.getElementById('sadf_asgjhasasqw12');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide13() {
+    var wor = document.getElementById('sadf_asgjhasasqw13');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide14() {
+    var wor = document.getElementById('sadf_asgjhasasqw14');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide15() {
+    var wor = document.getElementById('sadf_asgjhasasqw15');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function lfslicls() {
+    let drclc = document.getElementsByClassName("leftsideslide_asdfka")[0]
+    drclc.style.display = "none"
+    document.getElementsByClassName("righansdside_box054")[0].style.position = "relative"
+}
+function leftsldopen() {
+    let rightbxfix = document.getElementsByClassName("righansdside_box054")[0]
+    rightbxfix.style.position = "fixed"
+ 
+
+    let lefsdopn = document.getElementsByClassName("leftsideslide_asdfka")[0]
+    lefsdopn.style.display = "inline-flex";
+ //   lefsdopn.style.position = "fixed";
+}
+function sliderleftcls() {
+    let rightbxrl = document.getElementsByClassName("righansdside_box054")[0]
+   rightbxrl.style.position = "relative"
+
+    let lefsdc = document.getElementsByClassName("leftsideslide_asdfka")[0]
+    lefsdc.style.display = "none"
+}
+
+// mic js sart
+function microclicka(){
+    document.getElementsByTagName("body")[0].style.position ="fixed"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="grid"
+    
+}
+
+function clomicd(){
+    document.getElementsByTagName("body")[0].style.position ="relative"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="none"
+}
+function clsmicd(){
+    document.getElementsByTagName("body")[0].style.position ="relative"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="none"
+}
+function btnclsmic(){
+    document.getElementsByTagName("body")[0].style.position ="relative"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="none"
+}
+
+// mic js close
+
+// appdivopen
+function appdvopn(){
+    let diappbtn =   document.getElementsByClassName("opnappdiv_jksfd24sdfkj")[0]
+    if(diappbtn.style.display="none"){
+        diappbtn.style.display="block";
+    }
+    else{
+        diappbtn.style.display="none";
+    }
+}
+
+// tempor
+
+function clsappdiv(){
+    document.getElementsByClassName("opnappdiv_jksfd24sdfkj")[0].style.display="none"
+}
+// appdivjsclose
---
+++ js/video page.js
@@ -0,0 +1,382 @@
+/*
+function colorred() {
+    
+    let rdcr = document.getElementById("fdjh_sadggh");
+    if (rdcr.style.color == "black"){
+        rdcr.style.color = "yellow";
+    }
+    else {
+        rdcr.style.color = "red";
+    }
+}
+function colorred2() {
+    let rdcr2 = document.getElementById("fdjh_sadggh2");
+    if (rdcr2.style.color == "black"){
+        rdcr2.style.color = "yellow";
+    }
+    else {
+        rdcr2.style.color = "red";
+    }
+}
+function colorred3() {
+    let rdcr3 = document.getElementById("fdjh_sadggh3");
+    if (rdcr3.style.color == "black"){
+        rdcr3.style.color = "yellow";
+    }
+    else {
+        rdcr3.style.color = "red";
+    }
+}
+function colorred4() {
+    let rdcr4 = document.getElementById("fdjh_sadggh4");
+    if (rdcr4.style.color == "black"){
+        rdcr4.style.color = "yellow";
+    }
+    else {
+        rdcr4.style.color = "red";
+    }
+}
+function colorred5() {
+    let rdcr5 = document.getElementById("fdjh_sadggh5");
+    if (rdcr5.style.color == "black"){
+        rdcr5.style.color = "yellow";
+    }
+    else {
+        rdcr5.style.color = "red";
+    }
+}
+*/
+
+function closeadd() {
+    var btncl = document.getElementsByClassName("youtubedgjsa_0154")[0]
+    btncl.style.display = "none";
+}
+
+function ShowAndHide() {
+    var wor = document.getElementById('sadf_asgjhasasqw');
+    // document.getElementById("hidshowjafsdg_dgha").style.background = "red";
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+
+
+}
+
+function ShowAndHide2() {
+    var wor = document.getElementById('sadf_asgjhasasqw2');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide3() {
+    var wor = document.getElementById('sadf_asgjhasasqw3');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide4() {
+    var wor = document.getElementById('sadf_asgjhasasqw4');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide5() {
+    var wor = document.getElementById('sadf_asgjhasasqw5');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide6() {
+    var wor = document.getElementById('sadf_asgjhasasqw6');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide7() {
+    var wor = document.getElementById('sadf_asgjhasasqw7');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide8() {
+    var wor = document.getElementById('sadf_asgjhasasqw8');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide9() {
+    var wor = document.getElementById('sadf_asgjhasasqw9');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide10() {
+    var wor = document.getElementById('sadf_asgjhasasqw10');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide11() {
+    var wor = document.getElementById('sadf_asgjhasasqw11');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide12() {
+    var wor = document.getElementById('sadf_asgjhasasqw12');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide13() {
+    var wor = document.getElementById('sadf_asgjhasasqw13');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide14() {
+    var wor = document.getElementById('sadf_asgjhasasqw14');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function ShowAndHide15() {
+    var wor = document.getElementById('sadf_asgjhasasqw15');
+    if (wor.style.display == 'none') {
+        wor.style.display = 'block';
+    } else {
+        wor.style.display = 'none';
+    }
+}
+
+function lfslicls() {
+    let drclc = document.getElementsByClassName("leftsideslide_asdfka")[0]
+    drclc.style.display = "none"
+    document.getElementsByClassName("right_fgjksdf")[0].style.position = "relative"
+}
+function leftsldopen() {
+    let rightbxfix = document.getElementsByClassName("right_fgjksdf")[0]
+    //  rightbxfix.style.position = "fixed"
+
+
+    let lefsdopn = document.getElementsByClassName("leftsideslide_asdfka")[0]
+    lefsdopn.style.display = "inline-flex";
+    //   lefsdopn.style.position = "fixed";
+}
+function sliderleftcls() {
+    let rightbxrl = document.getElementsByClassName("right_fgjksdf")[0]
+    //   rightbxrl.style.position = "relative"
+
+    let lefsdc = document.getElementsByClassName("leftsideslide_asdfka")[0]
+    lefsdc.style.display = "none"
+}
+
+
+//temporary see only time /////////////////
+timesearcbar = () => {
+    document.getElementById("sdfajkgb_gfh0274").value = (new Date())
+}
+timea = setInterval(timesearcbar, 10);
+
+function subunsub() {
+
+    document.getElementsByClassName("like_014sdafvideobell")[0].style.display = "inline-flex"
+    sub1value1 = document.getElementsByClassName("like_014sdafvideosuba")[0].style.display = "none"
+    /*    let subun = document.getElementsByClassName("like_014sdafvideosuba")[0]
+       if (subun.innerHTML = "subscribe") {
+           subun.innerHTML = "unsubscribe";
+           subun.style.background = "grey";
+       }
+       
+       else {
+           subun.innerHTML = "unsubscribe"
+       }
+       */
+
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.subscribers) {
+            localStorage.subscribers = Number(localStorage.subscribers) + 1;
+        }
+        else {
+            localStorage.subscribers = 1;
+        }
+        document.getElementsByClassName("view_chancel_subsrcicer")[0].innerHTML = +localStorage.subscribers + " Subscribers"
+    }
+
+}
+
+
+function removecoment() {
+    document.getElementsByClassName("asfhj_asdkj0567465")[0].style.display = "none"
+}
+function microclicka() {
+    alert("microphone not allow || comming soon");
+}
+
+
+function showcmnt1() {
+    document.getElementsByClassName("cmt_jksdfuserpsaa")[0].style.height = "100%";
+    document.getElementById("showmor_cmnt").style.display = "none";
+}
+
+function subun() {
+
+
+    document.getElementsByClassName("asdfh_sdjkblackkhgf")[0].style.display = "grid";
+    document.getElementsByClassName("asdfh_sdjkblackkhgf")[0].style.position = "fixed";
+    document.getElementsByTagName("body")[0].style.position = "fixed";
+
+}
+
+function cancelnoch() {
+    document.getElementsByClassName("asdfh_sdjkblackkhgf")[0].style.display = "none";
+    document.getElementsByTagName("body")[0].style.position = "relative";
+}
+
+function cancelnochun() {
+    // if (typeof (Storage) !== "undefined") {
+    //     if (localStorage.unsubscribe) {
+    //         localStorage.unsubscribe = Number(localStorage.subscribers) - 1;
+    //     }
+    //     else {
+    //         localStorage.unsubscribe = 1;
+    //     }
+    //     document.getElementsByClassName("view_chancel_subsrcicer")[0].innerHTML = +localStorage.unsubscribe + "Subscribers"
+    // }
+
+    document.getElementsByClassName("like_014sdafvideobell")[0].style.display = "none"
+    document.getElementsByClassName("asdfh_sdjkblackkhgf")[0].style.display = "none";
+    document.getElementsByTagName("body")[0].style.position = "relative";
+    document.getElementsByClassName("like_014sdafvideosuba")[0].style.display = "block"
+}
+
+function likeinc() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        }
+        else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("asdf0df_asdn015_a1").innerHTML = +localStorage.clickcount + " Like"
+    }
+}
+function dislikeinc() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.dislikecount) {
+            localStorage.dislikecount = Number(localStorage.dislikecount) + 1;
+        }
+        else {
+            localStorage.dislikecount = 1;
+        }
+        document.getElementById("asdf0df_asdn015_a2").innerHTML = +localStorage.dislikecount + " Dislike"
+    }
+
+}
+
+function btnbell(){
+    document.getElementsByClassName("dsfj_khsadf024asdfjh")[0].style.display ="grid"
+}
+
+function bellicosl1(){
+    document.getElementsByClassName("dsfj_khsadf024asdfjh")[0].style.display="none"
+    let iconb = document.getElementById("sne_casdfom_click_btn1");
+    document.getElementsByClassName("like_014sdafvideobell")[0].innerHTML = iconb
+}
+function bellicosl2(){
+    document.getElementsByClassName("dsfj_khsadf024asdfjh")[0].style.display="none"
+    let iconb2 = document.getElementById("sne_casdfom_click_btn2");
+    document.getElementsByClassName("like_014sdafvideobell")[0].innerHTML = iconb2
+}
+function bellicosl3(){
+    document.getElementsByClassName("dsfj_khsadf024asdfjh")[0].style.display="none"
+    let iconb3 = document.getElementById("sne_casdfom_click_btn3");
+    document.getElementsByClassName("like_014sdafvideobell")[0].innerHTML = iconb3
+}
+ 
+function fuldesvid(){
+    document.getElementsByClassName("kjsdfjk_asdjkf0215")[0].style.height = "auto";
+}
+
+
+// mic js sart
+function microclicka(){
+    document.getElementsByTagName("body")[0].style.position ="fixed"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="grid"
+    
+}
+
+function clomicd(){
+    document.getElementsByTagName("body")[0].style.position ="relative"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="none"
+}
+function clsmicd(){
+    document.getElementsByTagName("body")[0].style.position ="relative"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="none"
+}
+function btnclsmic(){
+    document.getElementsByTagName("body")[0].style.position ="relative"
+    document.getElementsByClassName("jksdf_kdsfg0524dfgjh")[0].style.display="none"
+}
+
+// mic js close
+
+// appdivopen
+function appdvopn(){
+    let diappbtn =   document.getElementsByClassName("opnappdiv_jksfd24sdfkj")[0]
+    if(diappbtn.style.display="none"){
+        diappbtn.style.display="block";
+    }
+    else{
+        diappbtn.style.display="none";
+    }
+}
+
+// tempor
+
+function clsappdiv(){
+    document.getElementsByClassName("opnappdiv_jksfd24sdfkj")[0].style.display="none"
+}
+// appdivjsclose
+
